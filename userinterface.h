9aeb52d0a040e284d17b5c6b17affed7b590bbcc
MaestroFries <44793100+MaestroFries@users.noreply.github.com>
2022-04-10 22:31:21 -0700

    Create userinterface.h

---
+++ userinterface.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "iostream"
+
+
+bool state;
+void titleScreen()
+{
+	std::cout << " .d8888b.           888                                 .d8888b.                    888                             " << std::endl;
+	std::cout << "d88P  Y88b          888                                d88P  Y88b                   888                             " << std::endl;
+	std::cout << "888    888          888                                888    888                   888                             " << std::endl;
+	std::cout << "888         .d88b.  888  .d88b.  888  888 888d888      888         8888b.  88888b.  888888 888  888 888d888 .d88b.  " << std::endl;
+	std::cout << "888        d88''88b 888 d88''88b 888  888 888P'        888            '88b 888 '88b 888    888  888 888P'  d8P  Y8b " << std::endl;
+	std::cout << "888    888 888  888 888 888  888 888  888 888          888    888 .d888888 888  888 888    888  888 888    88888888 " << std::endl;
+	std::cout << "Y88b  d88P Y88..88P 888 Y88..88P Y88b 888 888          Y88b  d88P 888  888 888 d88P Y88b.  Y88b 888 888    Y8b.     " << std::endl;
+	std::cout << "'Y8888P'    'Y88P'  888  'Y88P'   'Y88888 888           'Y8888P'  'Y888888 88888P'   'Y888  'Y88888 888     'Y8888  " << std::endl;
+	std::cout << "                                                                           888                                      " << std::endl;
+	std::cout << "                                                                           888                                      " << std::endl;
+	std::cout << "                                                                           888                                      " << std::endl;
+	std::cout << "____________________________________________________________________________________________________________________" << std::endl;
+	
+
+
+}
+
